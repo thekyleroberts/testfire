2774765eddab4b9049bf0d9832b9495cc1c881a9
thekyleroberts <kaynorm@hotmail.com>
2018-05-31 18:31:41 -0400

    landing page and Procfile for production test

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/landing'
+
+  root 'static_pages#landing'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
